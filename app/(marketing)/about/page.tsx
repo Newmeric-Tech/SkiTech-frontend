463bd3382250de0a668341a3ceff2e27824ba6e5
Nrup-04 <nupurpanchal18@gmail.com>
2026-05-14 15:59:33 +0530

    fix: add use client to marketing pages to resolve client-reference-manifest build error

--- app/(marketing)/about/page.tsx
+++ app/(marketing)/about/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import AboutUs from "@/components/marketing/AboutUs";
 
 export default function AboutPage() {
--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import Hero from "@/components/marketing/Hero";
 import { Stats } from "@/components/marketing/Stats";
 import AboutUs from "@/components/marketing/AboutUs";
